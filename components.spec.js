7cfe721a96a1f5c7c048959609dabc3bb5f89fce
Dong Vu <dongvu@Dongs-MacBook-Pro.local>
2018-01-12 09:37:52 +0700

    done test case

--- components.spec.js
+++ components.spec.js
@@ -1,98 +1,125 @@
 import React from "react";
-import {expect} from "chai";
-import Enzyme, {shallow, mount} from "enzyme";
+import { expect } from "chai";
+import Enzyme, { shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import {spy} from "sinon";
+import { spy } from "sinon";
 
-import App, {buttonName} from "./src/App";
+import App, { buttonName } from "./src/App";
 import Monitor from "./src/components/Monitor";
 import ButtonGroup from "./src/components/ButtonGroup";
 
-Enzyme.configure({adapter: new Adapter()});
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("App", () => {
   it("should render Monitor and ButtonGroup", () => {
-    const wrapper = shallow(<App/>);
-    expect(wrapper.containsAllMatchingElements([ < Monitor />, < ButtonGroup />
-    ]),)
-      .to
-      .equal(true);
+    const wrapper = shallow(<App />);
+    expect(
+      wrapper.containsAllMatchingElements([<Monitor />, <ButtonGroup />]),
+    ).to.equal(true);
   });
 
   it("should start with empty", () => {
-    const wrapper = shallow(<App/>);
-    expect(wrapper.state("display"))
-      .to
-      .equal("0");
-    expect(wrapper.state("operator"))
-      .to
-      .equal("");
-    expect(wrapper.state("tmpOperator"))
-      .to
-      .equal("");
-    expect(wrapper.state("tmp"))
-      .to
-      .equal("0");
-    expect(wrapper.state("sum"))
-      .to
-      .equal("");
+    const wrapper = shallow(<App />);
+    expect(wrapper.state("display")).to.equal("0");
+    expect(wrapper.state("operator")).to.equal("");
+    expect(wrapper.state("tmpOperator")).to.equal("");
+    expect(wrapper.state("tmp")).to.equal("0");
+    expect(wrapper.state("sum")).to.equal("");
   });
 
-  it("click on <plus button> should be plus", () => {
-    const wrapper = shallow(<App/>);
-    wrapper.setState({operator: "+", tmp: 0, sum: 1});
-    wrapper
-      .instance()
-      .clickHandle("+");
-    expect(wrapper.state("sum"))
-      .to
-      .equal(1);
+  it("click <AC Button> should reset to initial State", () => {
+    const wrapper = shallow(<App />);
+    wrapper.instance().clickHandle("AC");
+    expect(wrapper.state("display")).to.equal("0");
+    expect(wrapper.state("operator")).to.equal("");
+    expect(wrapper.state("tmpOperator")).to.equal("");
+    expect(wrapper.state("tmp")).to.equal("0");
+    expect(wrapper.state("sum")).to.equal("");
   });
 
-  it("click on <plus button> should change operator to plus", () => {
-    const wrapper = shallow(<App/>);
-    wrapper.setState({operator: "="});
-    wrapper
-      .instance()
-      .clickHandle("+");
-    expect(wrapper.state("operator"))
-      .to
-      .equal("+");
+  it("Click <C Button> should reset display", () => {
+    const wrapper = shallow(<App />);
+    wrapper.instance().clickHandle("C");
+    expect(wrapper.state("display")).to.equal("0");
   });
 
-  it("click on <minus button> should be minus", () => {
-    const wrapper = shallow(<App/>);
-    wrapper.setState({operator: "+", tmp: 0, sum: 1});
-    wrapper
-      .instance()
-      .clickHandle("+");
-    expect(wrapper.state("sum"))
-      .to
-      .equal(1);
+  it("Click <% Button> should devide display number for 100", () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({ display: "999" });
+    wrapper.instance().clickHandle("%");
+    expect(wrapper.state("display")).to.equal(9.99);
   });
 
-  // it("click on <minus button> should change operator to plus", () => {   const
-  // wrapper = shallow(<App />);   wrapper.setState({     operator: "=",   });
-  // wrapper.instance().clickHandle("+");
-  // expect(wrapper.state("operator")).to.equal("+"); });
+  it("Click <+/- Button> should switch betwen negative / positive", () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({ display: "-999" });
+    wrapper.instance().clickHandle("+/-");
+    expect(wrapper.state("display")).to.equal("999");
+  });
+
+  it("Click <Number Button> should concat with early clicked", () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({ display: "99" });
+    wrapper.instance().clickHandle("9");
+    expect(wrapper.state("display")).to.equal("999");
+  });
+
+  it("Click <Number Button> after click equal operator", () => {
+    const wrapper = shallow(<App />);
+  });
+
+  it("Click on <plus Button> should change tmpOperator to plus", () => {
+    const wrapper = shallow(<App />);
+    wrapper.instance().clickHandle("+");
+    expect(wrapper.state("tmpOperator")).to.equal("+");
+  });
+
+  it("Click on <minus button> should change tmpOperator to minus", () => {
+    const wrapper = shallow(<App />);
+    wrapper.instance().clickHandle("-");
+    expect(wrapper.state("tmpOperator")).to.equal("-");
+  });
+
+  it("Click on <multiple button> should change tmpOperator to multiple", () => {
+    const wrapper = shallow(<App />);
+    wrapper.instance().clickHandle("*");
+    expect(wrapper.state("tmpOperator")).to.equal("*");
+  });
+
+  it("Click on <devide Button> should change tmpOperator to devide", () => {
+    const wrapper = shallow(<App />);
+    wrapper.instance().clickHandle("/");
+    expect(wrapper.state("tmpOperator")).to.equal("/");
+  });
+
+  it("click on <operator button> should calculations & change tmpOperator to clicked operator", () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({ operator: "", tmpOperator: "+", tmp: 1, sum: 2 });
+    wrapper.instance().clickHandle("*");
+    expect(wrapper.state("sum")).to.equal(3);
+    expect(wrapper.state("tmpOperator")).to.equal("*");
+  });
+
+  it("click on <dot button> should display 1 time", () => {
+    const wrapper = shallow(<App />);
+    wrapper.setState({ dotCount: 1, display: "9.9" });
+    wrapper.instance().clickHandle(".");
+    expect(wrapper.state("display")).to.equal("9.9");
+  });
 });
 
 describe("Monitor", () => {
   it("Monitor should render undefined", () => {
-    const wrapper = shallow(<Monitor value={undefined}/>);
+    const wrapper = shallow(<Monitor value={undefined} />);
     const monitorDIV = wrapper.find("div.monitor");
-    expect(monitorDIV.text())
-      .to
-      .equal("");
+    expect(monitorDIV.text()).to.equal("");
   });
 });
 
 describe("ButtonGroup", () => {
   it("should have all required buttons", () => {
-    const wrapper = shallow(<ButtonGroup buttonName={buttonName}/>);
+    const wrapper = shallow(<ButtonGroup buttonName={buttonName} />);
     const buttons = wrapper.findWhere(n => n.name() === "Button");
-    expect(buttons.length)
-      .to
-      .equal(19);
+    expect(buttons.length).to.equal(19);
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Monitor from "./components/Monitor";
 import ButtonGroup from "./components/ButtonGroup";
-import {log} from "util";
+import { log } from "util";
 
 if (process.env.NODE_ENV === "development") {
   require("./App.css");
@@ -13,7 +13,7 @@ const container = {
   height: "320px",
   border: "1px solid #000000",
   borderRadius: "5px",
-  boxShadow: "1px 1px 1px #000000"
+  boxShadow: "1px 1px 1px #000000",
 };
 
 export const buttonName = [
@@ -35,7 +35,7 @@ export const buttonName = [
   "+",
   "0",
   ".",
-  "="
+  "=",
 ];
 function calcutationCase(tmpOperator, sum, tmp) {
   var result;
@@ -65,25 +65,18 @@ class App extends Component {
       tmpOperator: "", // Toán tử click cuối cùng.
       tmp: "0", // Toán hạng cuối cùng.
       sum: "", // Kết quả phép tính.
-      dotCount: 0
+      dotCount: 0,
     };
   }
 
   clickHandle = clickedValue => {
     var result;
-    var {
-      sum,
-      display,
-      tmp,
-      operator,
-      tmpOperator,
-      dotCount
-    } = this.state;
+    var { sum, display, tmp, operator, tmpOperator, dotCount } = this.state;
     switch (clickedValue) {
       case "C":
         // Khi ấn nút 'C' thì giá trị trên màn hình (display) sẽ trở về '0' và giữ
         // nguyên kết quả tính toán trước (sum) và chuyển nút 'C' thành 'AC'.
-        this.setState({display: "0"});
+        this.setState({ display: "0" });
         buttonName[0] = "AC";
         break;
 
@@ -96,7 +89,7 @@ class App extends Component {
           tmpOperator: "",
           tmp: "0",
           sum: "",
-          dotCount: 0
+          dotCount: 0,
         });
         buttonName[0] = "AC";
         break;
@@ -105,80 +98,71 @@ class App extends Component {
         if (operator === "" && sum !== "") {
           // Trường hợp thay đổi toán tử  so với ban đầu (vd: đang + chuyển sang -).
           result = calcutationCase(tmpOperator, sum, tmp);
-          this.setState({sum: result, display: result, tmp: result});
-        } else if (operator === "") {
-          // Trường hợp sau khi click số ta click button toán tử.
-          this.setState({
-            sum: Number(sum) + Number(tmp),
-            // Kết quả mới = kết quả cũ  +  toán hạng cuối cùng nhập vào.
-            display: Number(sum) + Number(tmp)
-          });
+          this.setState({ sum: result, display: result, tmp: result });
+          this.setState({ operator: "+", tmpOperator: "+", tmp: display });
+          break;
         }
-
-        // Trong mọi trường hợp operator & tmpOperator đều được set = '+' sau khi click.
-        // Và set lại giá trị cho biến tạm = giá trị đang hiển thị
-        this.setState({operator: "+", tmpOperator: "+", tmp: display});
+        // Trường hợp sau khi click số ta click button toán tử.
+        this.setState({
+          sum: Number(sum) + Number(tmp),
+          // Kết quả mới = kết quả cũ  +  toán hạng cuối cùng nhập vào.
+          display: Number(sum) + Number(tmp),
+        });
+        this.setState({ operator: "+", tmpOperator: "+", tmp: display });
         break;
 
       case "-":
         if (operator === "" && sum !== "") {
           result = calcutationCase(tmpOperator, sum, tmp);
-          this.setState({sum: result, display: result, tmp: result});
+          this.setState({ sum: result, display: result, tmp: result });
         } else if (operator === "") {
           this.setState({
-            sum: sum === ""
-              ? Number(tmp)
-              : Number(sum) - Number(tmp),
-            display: sum === ""
-              ? Number(tmp)
-              : Number(sum) - Number(tmp)
+            sum: sum === "" ? Number(tmp) : Number(sum) - Number(tmp),
+            display: sum === "" ? Number(tmp) : Number(sum) - Number(tmp),
           });
         }
-        this.setState({operator: "-", tmpOperator: "-", tmp: display});
+        this.setState({ operator: "-", tmpOperator: "-", tmp: display });
         break;
 
       case "*":
         if (operator === "" && sum !== "") {
           result = calcutationCase(tmpOperator, sum, tmp);
-          this.setState({sum: result, display: result, tmp: result});
+          this.setState({ sum: result, display: result, tmp: result });
         } else if (operator === "") {
           this.setState({
-            sum: sum === ""
-              ? Number(tmp)
-              : Number(sum) * Number(tmp),
-            display: sum === ""
-              ? Number(tmp)
-              : Number(sum) * Number(tmp)
+            sum: sum === "" ? Number(tmp) : Number(sum) * Number(tmp),
+            display: sum === "" ? Number(tmp) : Number(sum) * Number(tmp),
           });
         }
-        this.setState({operator: "*", tmpOperator: "*", tmp: display});
+        this.setState({ operator: "*", tmpOperator: "*", tmp: display });
         break;
 
       case "/":
         if (operator === "" && sum !== "") {
           result = calcutationCase(tmpOperator, sum, tmp);
-          this.setState({sum: result, display: result, tmp: result});
+          this.setState({ sum: result, display: result, tmp: result });
         } else if (operator === "") {
           this.setState({
-            sum: sum === ""
-              ? Number(tmp)
-              : Number(sum) / Number(tmp),
-            display: sum === ""
-              ? Number(tmp)
-              : Number(sum) / Number(tmp)
+            sum: sum === "" ? Number(tmp) : Number(sum) / Number(tmp),
+            display: sum === "" ? Number(tmp) : Number(sum) / Number(tmp),
           });
         }
-        this.setState({operator: "/", tmpOperator: "/", tmp: display});
+        this.setState({ operator: "/", tmpOperator: "/", tmp: display });
         break;
 
       case "=":
         if (tmpOperator === "") {
-          this.setState({sum: display, operator: "="});
+          this.setState({ sum: display, operator: "=" });
           break;
         }
 
         result = calcutationCase(tmpOperator, sum, tmp);
-        this.setState({sum: result, display: result, operator: "="});
+        this.setState({
+          sum: result,
+          display: result,
+          operator: "=",
+          dotCount: 1,
+        });
         break;
 
       case ".":
@@ -187,25 +171,24 @@ class App extends Component {
         if (dotCount === 0) {
           this.setState({
             display: display + clickedValue,
-            dotCount: 1
+            dotCount: 1,
           });
         }
         break;
 
       case "%":
         this.setState({
-          sum: Number(display) / 100,
-          display: Number(display) / 100
+          display: Number(display) / 100,
         });
         break;
       case "+/-":
         if (Number(display) > 0) {
           this.setState({
-            display: "-" + display
+            display: "-" + display,
           });
         } else {
           this.setState({
-            display: display.slice(1)
+            display: display.slice(1),
           });
         }
         break;
@@ -215,39 +198,75 @@ class App extends Component {
         buttonName[0] = "C";
         // Click số sau khi click '='
         if (operator === "=") {
-          this.setState({
-            display: clickedValue,
-            operator: "0",
-            tmpOperator: "",
-            tmp: "0",
-            sum: clickedValue,
-            dotCount: 0
-          }, () => {
-            console.log("sum: " + this.state.sum + "\ntmp: " + this.state.tmp + "\ndisplay: " + this.state.display + "\n operator: " + this.state.operator,);
-          });
+          this.setState(
+            {
+              display: clickedValue,
+              operator: "0",
+              tmpOperator: "",
+              tmp: "0",
+              sum: clickedValue,
+              dotCount: 0,
+            },
+            () => {
+              console.log(
+                "sum: " +
+                  this.state.sum +
+                  "\ntmp: " +
+                  this.state.tmp +
+                  "\ndisplay: " +
+                  this.state.display +
+                  "\n operator: " +
+                  this.state.operator,
+              );
+            },
+          );
         } else if (display === "0" || operator !== "") {
           // 1. Nếu màn hình đang hiển thị số '0' thì bỏ số '0' và hiển thị (display) giá
           // trị vừa click đồng thời gán giá trị đó cho biến tạm (temp).
           // 2. Sau khi click toán tử (+,-,*,/) thì số được click sẽ là toán hạng mới.
-          this.setState({
-            display: clickedValue,
-            tmp: clickedValue,
-            dotCount: 0,
-            operator: ""
-          }, () => {
-            console.log("sum: " + this.state.sum + "\ntmp: " + this.state.tmp + "\ndisplay: " + this.state.display + "\n operator: " + this.state.operator,);
-          });
+          this.setState(
+            {
+              display: clickedValue,
+              tmp: clickedValue,
+              dotCount: 0,
+              operator: "",
+            },
+            () => {
+              console.log(
+                "sum: " +
+                  this.state.sum +
+                  "\ntmp: " +
+                  this.state.tmp +
+                  "\ndisplay: " +
+                  this.state.display +
+                  "\n operator: " +
+                  this.state.operator,
+              );
+            },
+          );
         } else {
           // Ngược lại ta cộng chuỗi giá trị vừa click với chuỗi trước đang hiển thị trên
           // mành hình (display) và gán ngược trở lại cho biến tạm (tmp) & biến hiển thị
           // (display)
-          this.setState({
-            display: display + clickedValue,
-            tmp: display + clickedValue,
-            operator: ""
-          }, () => {
-            console.log("sum: " + this.state.sum + "\ntmp: " + this.state.tmp + "\ndisplay: " + this.state.display + "\n operator: " + this.state.operator,);
-          });
+          this.setState(
+            {
+              display: display + clickedValue,
+              tmp: display + clickedValue,
+              operator: "",
+            },
+            () => {
+              console.log(
+                "sum: " +
+                  this.state.sum +
+                  "\ntmp: " +
+                  this.state.tmp +
+                  "\ndisplay: " +
+                  this.state.display +
+                  "\n operator: " +
+                  this.state.operator,
+              );
+            },
+          );
         }
     }
   };
@@ -255,8 +274,8 @@ class App extends Component {
   render() {
     return (
       <div style={container}>
-        <Monitor value={this.state.display}/>
-        <ButtonGroup buttonName={buttonName} onClick={this.clickHandle}/>
+        <Monitor value={this.state.display} />
+        <ButtonGroup buttonName={buttonName} onClick={this.clickHandle} />
       </div>
     );
   }
